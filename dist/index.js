eb6b137439d66bcf356a553d9d3a050312214c23
Philip Wee <philip.andrewweedewang@gmail.com>
2021-11-19 11:08:54 +0800

    Initial commit

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,28 @@
 "use strict";
-const target = {
-    msg1: "Hello",
-    msg2: "World",
-    yourMotherFunc() {
-        console.log("Yo mama");
+Object.defineProperty(exports, "__esModule", { value: true });
+class VirtualProxyWrapper {
+    constructor(target) {
+        this.handler = this.createHandler();
+        this.proxy = new Proxy(target, this.handler);
+        this.callQueue = [];
+        return this;
     }
-};
-const proxy = new Proxy(target, {});
-proxy.yourMotherFunc();
+    createHandler() {
+        const that = this;
+        return {
+            get: function (target, prop, receiver) {
+                if (typeof Reflect.get(target, prop) === 'function') {
+                    const origMethod = Reflect.get(target, prop);
+                    return function (...args) {
+                        that.callQueue.push(() => origMethod.apply(receiver, args));
+                    };
+                }
+                throw "Only deferred method calls are allowed";
+            }
+        };
+    }
+    execute() {
+        this.callQueue.forEach((func) => func());
+    }
+}
+exports.default = VirtualProxyWrapper;
